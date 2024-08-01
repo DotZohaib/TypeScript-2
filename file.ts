7a1bd01ea3da76eb32d381be31bd03ca5b515e18
ZOHAIB ALI DAYO <154052553+DotZohaib@users.noreply.github.com>
2024-08-01 23:32:10 +0500

    Add files via upload
    TypeScript-2

---
+++ file.ts
@@ -0,0 +1,21 @@
+// interface Slip{
+//  customerName: String,
+//  timestamp: number
+// }
+
+// let bill:Slip={
+//     customerName: "junaid",
+//  timestamp: 234
+// }
+
+// console.log(bill.customerName, bill.timestamp)
+
+
+export enum shift{
+    Morning = "9 to 12 ",
+    After_noon = "1 to 5",
+    Evening = "6 to 8",
+    night = "9 to 5"
+}
+
+console.log(shift.After_noon)
